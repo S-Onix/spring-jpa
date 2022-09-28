33f18d97936d921978848f75da79269d558310b2
S-Onix <msyoo101@hansung.ac.kr>
2022-09-28 17:31:13 +0900

    jpa method test

--- bookmanager/src/test/java/com/fc/jpa/bookmanager/repository/UserRepositoryTest.java
+++ bookmanager/src/test/java/com/fc/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -44,5 +44,31 @@ class UserRepositoryTest {
         userRepository.getOne(1L);
         User user = userRepository.findById(1L).orElse(null); //findById는 Optional로 Wrapping 되어 있음, orElse 를 붙이게 되면 Entity 객체 타입으로 변경해야함
         System.out.println(user.toString());
+
+    }
+
+    @Test
+    public void saveAndFlush(){
+        userRepository.saveAndFlush(new User("test", "test@namver.com"));
+        userRepository.flush(); //DB 반영시점을 변경하는 역할
+
+    }
+
+    @Test
+    public void count(){
+        long count = userRepository.count();
+        System.out.println(count);
+    }
+
+    @Test
+    public void exist(){ //count 쿼리를 통해 처리됨
+        boolean isExist = userRepository.existsById(1L);
+        System.out.println(isExist);
+    }
+
+    @Test
+    public void delete(){
+        userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
+        
     }
 }
